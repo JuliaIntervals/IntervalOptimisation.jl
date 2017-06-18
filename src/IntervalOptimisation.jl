297bcfebf1587e11cf0bc3dc2a5385f2304c1ae2
David Sanders <dpsanders@gmail.com>
2017-06-18 06:52:39 -0500

    Fix inf/infimum

--- src/IntervalOptimisation.jl
+++ src/IntervalOptimisation.jl
@@ -9,6 +9,14 @@ using .SortedVectors
 
 using IntervalArithmetic, IntervalRootFinding
 
+if !isdefined(:sup)
+    const sup = supremum
+end
+
+if !isdefined(:inf)
+    const inf = infimum
+end
+
 include("optimise.jl")
 
 end
