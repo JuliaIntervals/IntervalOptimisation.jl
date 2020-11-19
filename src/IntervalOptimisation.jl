130179b2a4fdafd46090703f82abc61ea5ea3fcb
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2020-11-19 18:53:42 +0100

    rebase master

--- src/IntervalOptimisation.jl
+++ src/IntervalOptimisation.jl
@@ -1,5 +1,8 @@
 module IntervalOptimisation
 
+using IntervalArithmetic, IntervalRootFinding
+
+
 export minimise, maximise,
        minimize, maximize
 export HeapedVector, SortedVector
@@ -7,15 +10,16 @@ export HeapedVector, SortedVector
 include("StrategyBase.jl")
 using .StrategyBase
 
+export mean_value_form_scalar, third_order_taylor_form_scalar
+
 include("SortedVectors.jl")
 using .SortedVectors
 
 include("HeapedVectors.jl")
 using .HeapedVectors
 
-using IntervalArithmetic
-
 include("optimise.jl")
+include("centered_forms.jl")
 
 
 const minimize = minimise
